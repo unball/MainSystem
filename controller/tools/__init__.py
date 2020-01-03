3f6670587def352079e7cd165cc8c62b0ef62358
Luiz <labm1997@gmail.com>
2020-01-02 21:45:54 -0300

    Documentação nas ferramentas em controller/tools

--- controller/tools/__init__.py
+++ controller/tools/__init__.py
@@ -1,15 +1,19 @@
 import numpy as np
 
 def norm(p0: tuple, p1: tuple):
+  """Calcula a distância entre as tuplas `p0` e `p1` no plano"""
   return np.sqrt((p1[0]-p0[0])**2+(p1[1]-p0[1])**2)
 
 def ang(p0: tuple, p1: tuple):
+  """Calcula o ângulo entre as tuplas `p0` e `p1` no plano, este ângulo está em \\((-\\pi,\\pi]\\)"""
   return np.arctan2(p1[1]-p0[1], p1[0]-p0[0])
 
 def angl(p0: tuple):
+  """Calcula o ângulo da tupla `p0` no plano este ângulo está em \\((-\\pi,\\pi]\\)"""
   return np.arctan2(p0[1], p0[0])
 
 def sat(x: float, amp: float):
+  """Satura um número real `x` entre `amp` e `-amp`"""
   return max(min(x, amp), -amp)
 
 def fixAngle(angle: float):
