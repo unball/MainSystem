be093e22ea3198cbeaaf06814cf13c1000722739
Luiz <labm1997@gmail.com>
2020-01-18 19:05:39 -0300

    Retirado parâmetro desnecessário

--- controller/control/UFC.py
+++ controller/control/UFC.py
@@ -7,7 +7,7 @@ import time
 class UFC(HLC):
   """Controle unificado para o Univector Field, utiliza o ângulo definido pelo campo como referência \\(\\theta_d\\)."""
   def __init__(self, source):
-    super().__init__("Univector Field Control", source + "_UFC", {"kw": 1, "kp": 10, "L": 0.075, "vmax": 0.7, "mu": 0.24, "motorangaccelmax": 999, "r": 0.03, "tau": 0.100, "vref": 0})
+    super().__init__("Univector Field Control", source + "_UFC", {"kw": 1, "kp": 10, "L": 0.075, "vmax": 0.7, "mu": 0.24, "motorangaccelmax": 999, "r": 0.03, "vref": 0})
 
     self.g = 9.8
 
@@ -30,7 +30,6 @@ class UFC(HLC):
     vmax = self.getParam("vmax")
     motorangaccelmax = self.getParam("motorangaccelmax")
     r = self.getParam("r")
-    tau = self.getParam("tau")
     vref = self.getParam("vref")
       
     # Computa os erros
