4898632969d1f3257336e6dabbe07aaa31738960
André <andredornelas23@gmail.com>
2020-02-17 19:37:01 -0300

    Adição do método insideGoalArea

--- controller/world/ball.py
+++ controller/world/ball.py
@@ -1,5 +1,6 @@
 from controller.world.element import Element
 from controller.tools.kalman import KalmanFilter
+from controller.tools import insideRect
 
 def shift(data, array):
     return [data] + array[1:]
@@ -9,3 +10,7 @@ class Ball(Element):
 
   def __init__(self, world):
     super().__init__(world)
+
+  def insideGoalArea(self):
+    """Retorna se a bola está na área do gol aliado ou não"""
+    return insideRect(self.pos, self.world.allyGoalPos, self.world.goalAreaSize)
