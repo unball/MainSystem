152ef03b13af71569b2575abf7b8d9730f3d2133
Gabriel <gabrielbaliza1@gmail.com>
2020-02-19 12:11:56 -0300

    Primeira versão do MidFielder

--- controller/strategy/entity.py
+++ controller/strategy/entity.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from controller.strategy.movements import goToBall, goToGoal, projectBall, howFrontBall, howPerpBall, goalkeep, blockBallElipse
+from controller.strategy.movements import goToBall, goToGoal, projectBall, howFrontBall, howPerpBall, goalkeep, blockBallElipse, mirrorPosition
 from controller.strategy.field import UVFDefault, GoalKeeperField, DefenderField, UVFavoidGoalArea
 from controller.tools import ang, angError, norm, unit
 import numpy as np
@@ -145,3 +145,28 @@ class GoalKeeper(Entity):
             pose = goalkeep(rb, vb, rr, (rr[0], rg[1]))
             self.robot.field = GoalKeeperField(pose)
         #self.robot.field = UVFDefault(self.world, (rr[0], *pose[1:3]), rr, direction=0, spiral=False)
+        #else: self.robot.field = GoalKeeperField((rr[0], *pose[1:3]))
+        #self.robot.field = UVFDefault(self.world, pose, direction=0, radius=0.14)
+
+class MidFielder(Attacker, Entity):
+    def __init__(self, world, robot):
+        super().__init__(robot, (255,255,0))
+        
+        self.world = world
+
+    def directionDecider(self):
+        """Altera a propriedade 'dir' do robô de acordo com a decisão"""
+        # Inverte se o último erro angular foi maior que 90º
+        if abs(self.robot.lastAngError) > 90 * np.pi / 180:
+            self.robot.dir *= -1
+
+    def movementDecider(self, Attacker):
+        # Dados necessários para a decisão
+        rr = np.array(Attacker().robot.pose)
+        rb = np.array(self.world.ball.pos.copy())
+        rg = np.array(self.world.goalpos)
+
+        pose = mirrorPosition(rr, rb, rg)
+
+        self.robot.vref = 0
+        self.robot.field = UVFDefault(self.world, pose, rr, direction = 0, spiral = False)
