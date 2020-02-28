13e9b79c64ca992ff7d25532e9a3e364025e4d1e
Luiz <labm1997@gmail.com>
2020-02-28 20:52:40 -0300

    ESTADO DAS 20h52: Aumento do mu do Atacante (sem bolinha)

--- controller/world/__init__.py
+++ controller/world/__init__.py
@@ -44,7 +44,7 @@ class World(ParamsPattern):
     self.n_robots = 5
     self.fieldSide = Field.RIGHT
     self.running = False
-    self.mus = [0.07, 0.07, 0.10, 0.07, 0.07]
+    self.mus = [0.07, 0.07, 0.12, 0.07, 0.07]
     self.robots = [Robot(self, i, self.mus[i]) for i in range(self.n_robots)]
     self.enemyRobots = []
     self.edges = []
