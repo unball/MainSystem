2d437faa4218f9f9e78ca44fcfefb3ecf3a65d3f
Luiz <labm1997@gmail.com>
2020-02-29 17:41:22 -0300

    ESTADO DAS 17h41: (DEPOIS DA ULTIMA PARTIDA DO DIA 29/02)

--- controller/strategy/entity.py
+++ controller/strategy/entity.py
@@ -64,7 +64,7 @@ class Attacker(Entity):
 
         # Executa spin se estiver morto
         if not self.robot.isAlive():
-            self.robot.setSpin(-np.sign(rr[0]) if rr[1] > rb[1] else np.sign(rr[0]))
+            self.robot.setSpin(-np.sign(rr[0]) if rr[1] > 0 else np.sign(rr[0]))
             return
 
         # Bola projetada com offset
@@ -78,7 +78,7 @@ class Attacker(Entity):
 
         # Se estiver atrás da bola, estiver em uma faixa de distância "perpendicular" da bola, estiver com ângulo para o gol com erro menor que 30º vai para o gol
         #if howFrontBall(rb, rr, rg) < -0.03*(1-self.movState) and abs(howPerpBall(rb, rr, rg)) < 0.045 + self.movState*0.1 and abs(angError(ballGoalAngle, rr[2])) < (30+self.movState*60)*np.pi/180:
-        if howFrontBall(rb, rr, rg) < -0.03*(1-self.robot.movState) + 0.10*self.robot.movState and abs(angError(robotBallAngle, rr[2])) < (30+self.robot.movState*60)*np.pi/180 and np.abs(projectLine(rr[:2], unit(rr[2]), rg[0])) <= 0.15 + self.robot.movState*1:
+        if howFrontBall(rb, rr, rg) < -0.03*(1-self.robot.movState) + 0.10*self.robot.movState and (self.robot.movState == 1 or abs(howPerpBall(rb, rr, rg)) < 0.1) and abs(angError(robotBallAngle, rr[2])) < (30+self.robot.movState*60)*np.pi/180 and np.abs(projectLine(rr[:2], unit(rr[2]), rg[0])) <= 0.15 + self.robot.movState*0.2:
             #if howFrontBall(rb, rr, rg) < -0.03*(1-self.robot.movState) and abs(angError(robotBallAngle, rr[2])) < (30+self.robot.movState*60)*np.pi/180 and np.abs(projectLine(rr[:2], unit(rr[2]), rg[0])) <= 0.25:
             if self.robot.movState == 0:
                 self.robot.ref = (*(rr[:2] + 1000*unit(rr[2])), rr[2])
@@ -224,7 +224,7 @@ class MidFielder(Entity):
         robotBallAngle = ang(rr, rb)
 
         #if howFrontBall(rb, rr, rg) < -0.03*(1-self.movState) and abs(howPerpBall(rb, rr, rg)) < 0.045 + self.movState*0.1 and abs(angError(ballGoalAngle, rr[2])) < (30+self.movState*60)*np.pi/180:
-        if howFrontBall(rb, rr, rg) < -0.03*(1-self.robot.movState) + 0.10*self.robot.movState and abs(angError(robotBallAngle, rr[2])) < (30+self.robot.movState*60)*np.pi/180 and np.abs(projectLine(rr[:2], unit(rr[2]), rg[0])) <= 0.15 + self.robot.movState*1:
+        if howFrontBall(rb, rr, rg) < -0.03*(1-self.robot.movState) + 0.10*self.robot.movState and (self.robot.movState == 1 or abs(howPerpBall(rb, rr, rg)) < 0.1) and abs(angError(robotBallAngle, rr[2])) < (30+self.robot.movState*60)*np.pi/180 and np.abs(projectLine(rr[:2], unit(rr[2]), rg[0])) <= 0.15 + self.robot.movState*0.2:
             #if howFrontBall(rb, rr, rg) < -0.03*(1-self.robot.movState) and abs(angError(robotBallAngle, rr[2])) < (30+self.robot.movState*60)*np.pi/180 and np.abs(projectLine(rr[:2], unit(rr[2]), rg[0])) <= 0.25:
             if self.robot.movState == 0:
                 self.robot.ref = (*(rr[:2] + 1000*unit(rr[2])), rr[2])
