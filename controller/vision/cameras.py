c42723d3ff4120c95c264c903674f3395ef1781d
Luiz <labm1997@gmail.com>
2020-03-18 16:40:25 -0300

    Configuraçao para 120fps com a camera nova

--- controller/vision/cameras.py
+++ controller/vision/cameras.py
@@ -64,11 +64,12 @@ class CameraHandler():
     
     # Aloca a nova câmera
     if index != -1: 
+      #cap = cv2.VideoCapture("../visao/UnBall&RobotBulls1.webm")
       cap = cv2.VideoCapture(index)
-      cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M','J','P','G'))
-      cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-      cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-      cap.set(cv2.CAP_PROP_FPS, 120)
+      # cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M','J','P','G'))
+      # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+      # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+      # cap.set(cv2.CAP_PROP_FPS, 120)
       print(cap.get(cv2.CAP_PROP_FPS))
       self.__cap = cap
     
