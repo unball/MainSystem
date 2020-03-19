f8d84a895c97e19004293a62a808a44a9d694e5a
Luiz <labm1997@gmail.com>
2020-03-18 22:01:22 -0300

    Width, height e FPS podem ser gerenciados direto pela GUI

--- controller/vision/cameras.py
+++ controller/vision/cameras.py
@@ -2,31 +2,61 @@ import cv2
 import time
 from pkg_resources import resource_filename
 from os import listdir
+from threading import Lock
 
-from model.vision.camerasModel import CameraHandlerModel
+from model.paramsPattern import ParamsPattern
 
-class CameraHandler():
+class CameraHandler(ParamsPattern):
   """Classe que gerencia as câmeras do sistema permitindo rápida troca e retorno simples de frames"""
   def __init__(self):
+    ParamsPattern.__init__(self, "cameraParams", {
+      "current_camera": -1,
+      "frame_scale": 1,
+      "frame_width": 640,
+      "frame_height": 480,
+      "frame_fps": 30,
+      "frame_format": "YUYV"
+    })
+
     self.__cameras = []
     """Conjunto de câmeras disponíveis"""
-    
-    self.__model = CameraHandlerModel()
-    """Modelo `MainSystem.model.vision.camerasModel.CameraHandlerModel` que mantém as variáveis permanentes do gerenciador de câmeras"""
     
     self.__defaultFrame = cv2.imread(resource_filename(__name__, "defaultFrame.png"))
     """Armazena o frame padrão"""
     
     self.__cap = None
     """Armazena o `VideoCapture` atual"""
+
+    self.__lock = Lock()
     
   def setScale(self, scale: float):
     """Altera o valor da escala usada no frame"""
-    self.__model.frame_scale = scale
+    self.setParam("frame_scale", scale)
+
+  def setCameraParams(self, values):
+    self.__lock.acquire()
+    if self.__cap is None: return
+
+    self.setParam("frame_format", values["frame_format"])
+    self.setParam("frame_width", values["frame_width"])
+    self.setParam("frame_height", values["frame_height"])
+    self.setParam("frame_fps", values["frame_fps"])
+
+    self.setCamera(self.getParam("current_camera"), lock=False)
+
+    fourcc = int(self.__cap.get(cv2.CAP_PROP_FOURCC))
+    fourccstr = fourcc.to_bytes(length=4, byteorder='little').decode()
+
+    self.setParam("frame_format", fourccstr)
+    self.setParam("frame_width", int(self.__cap.get(cv2.CAP_PROP_FRAME_WIDTH)))
+    self.setParam("frame_height", int(self.__cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    self.setParam("frame_fps", int(self.__cap.get(cv2.CAP_PROP_FPS)))
+
+    self.__lock.release()
     
   def getScale(self):
     """Obtém o valor da escala usada no frame"""
-    return self.__model.frame_scale
+    return self.getParam("frame_scale")
     
   def scaleFrame(frame, scale: float):
     """Muda o tamanho, mantendo a proporção, de um frame de acordo com uma escala."""
@@ -36,29 +66,37 @@ class CameraHandler():
   def getFrame(self):
     """Retorna um frame no formato numpy (height, width, depth) com base na câmera atualmente selecionada."""
     try:
-      if self.__model.current_camera == -1:
+      if self.getParam("current_camera") == -1:
         time.sleep(0.001)
         if self.__defaultFrame is None: return None
-        return CameraHandler.scaleFrame(self.__defaultFrame, self.__model.frame_scale)
+        return CameraHandler.scaleFrame(self.__defaultFrame, self.getParam("frame_scale"))
+
+      self.__lock.acquire()
       
       if self.__cap is None:
-        self.setCamera(self.__model.current_camera)
+        self.setCamera(self.getParam("current_camera"), lock=False)
       
       if self.__cap is not None and self.__cap.isOpened():
         ret, frame = self.__cap.read()
         if frame is None: 
           time.sleep(0.001)
+          self.__lock.release()
           return None
-        return CameraHandler.scaleFrame(frame, self.__model.frame_scale)
-    except:
+        self.__lock.release()
+        return CameraHandler.scaleFrame(frame, self.getParam("frame_scale"))
+
+      self.__lock.release()
+    except Exception as e: 
+      print(e)
       print("Falha no módulo CameraHandler getFrame")
       time.sleep(0.001)
       return None
       
     return None
     
-  def setCamera(self, index):
+  def setCamera(self, index, lock=True):
     """Seleciona a câmera de índice `index`"""
+    if lock: self.__lock.acquire()
     # Libera a câmera em uso
     if self.__cap is not None: self.__cap.release()
     
@@ -66,18 +104,20 @@ class CameraHandler():
     if index != -1: 
       #cap = cv2.VideoCapture("../visao/UnBall&RobotBulls1.webm")
       cap = cv2.VideoCapture(index)
-      # cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M','J','P','G'))
-      # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-      # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-      # cap.set(cv2.CAP_PROP_FPS, 120)
-      print(cap.get(cv2.CAP_PROP_FPS))
+      cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*list(self.getParam("frame_format"))))
+      cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.getParam("frame_width"))
+      cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.getParam("frame_width"))
+      cap.set(cv2.CAP_PROP_FPS, self.getParam("frame_fps"))
+      
       self.__cap = cap
+
+    if lock: self.__lock.release()
     
-    self.__model.current_camera = index
+    self.setParam("current_camera", index)
     
   def getCamera(self):
     """Retorna o índice de câmera selecionado"""
-    return self.__model.current_camera
+    return self.getParam("current_camera")
     
   def updateCameras(self):
     """Atualiza a lista de câmeras detectadas"""
--- view/vision/camerasView.py
+++ view/vision/camerasView.py
@@ -28,12 +28,28 @@ class CameraHandlerView():
     
     self.__cameraPopoverAdjustment = builder.get_object("cameraPopoverAdjustment")
     """Referência ao objeto de interface gráfica `cameraPopoverAdjustment`, que controla a escala"""
+
+    self.__cameraFrameParamsUpdate = builder.get_object("cameraParamsUpdate")
+    """Referência ao objeto de interface gráfica `cameraParamsUpdate`, que quando clicado atualiza os parâmetros da câmera"""
+
+    self.__cameraFrameWidthEntry = builder.get_object("cameraPopoverWidth")
+    """Referência ao objeto de interface gráfica `cameraPopoverWidth`, que controla a largura do frame na fonte"""
+
+    self.__cameraFrameHeightEntry = builder.get_object("cameraPopoverHeight")
+    """Referência ao objeto de interface gráfica `cameraPopoverWidth`, que controla a altura do frame na fonte"""
+
+    self.__cameraFrameFPSEntry = builder.get_object("cameraPopoverFPS")
+    """Referência ao objeto de interface gráfica `cameraPopoverWidth`, que controla a taxa de frames na fonte"""
+
+    self.__cameraFrameFormatCombo = builder.get_object("cameraPopoverFormat")
+    """Referência ao objeto de interface gráfica `cameraPopoverFormat`, que controla o formato"""
     
     # Faz com que quando o botão é clicado apareça o cameraPopover
     self.__menuButton.set_popover(self.__cameraPopover)
     
     # Carrega o valor inicial da escala
     self.__cameraPopoverAdjustment.set_value(cameraHandler.getScale())
+    self.sync_values()
     
     # Adiciona a câmera "padrão" que é a imagem padrão
     self.add_to_list_box(self.__cameraPopoverCamList, -1, "Padrão")
@@ -42,11 +58,27 @@ class CameraHandlerView():
     self.__menuButton.connect("toggled", self.list_cameras, self.__cameraPopoverCamList)
     self.__cameraPopoverAdjustment.connect("value-changed", self.update_camera_scale)
     self.__cameraPopoverCamList.connect("row-selected", self.select_camera)
+    self.__cameraFrameParamsUpdate.connect("clicked", self.update_camera_params)
     
     
   def update_camera_scale(self, widget):
     """Agenda uma mudança de escala de acordo com o valor selecionado pelo `cameraPopoverAdjustment`"""
     self.__controller.addEvent(self.__cameraHandler.setScale, widget.get_value())
+
+  def sync_values(self):
+    self.__cameraFrameWidthEntry.set_text(str(self.__cameraHandler.getParam("frame_width")))
+    self.__cameraFrameHeightEntry.set_text(str(self.__cameraHandler.getParam("frame_height")))
+    self.__cameraFrameFPSEntry.set_text(str(self.__cameraHandler.getParam("frame_fps")))
+    self.__cameraFrameFormatCombo.set_active_id(self.__cameraHandler.getParam("frame_format"))
+
+  def update_camera_params(self, widget):
+    self.__controller.addEvent(self.__cameraHandler.setCameraParams, {
+      "frame_width": int(self.__cameraFrameWidthEntry.get_text()),
+      "frame_height": int(self.__cameraFrameHeightEntry.get_text()),
+      "frame_fps": int(self.__cameraFrameFPSEntry.get_text()),
+      "frame_format": self.__cameraFrameFormatCombo.get_active_id()
+    }, run_when_done_with_glib=(self.sync_values,))
+
       
   def add_to_list_box(self, widget_list, index, name):
     """Adiciona a um `widget_list` uma câmera com índice `index` e com nome `name`. Cria com ela selecionada caso o índice for igual ao índice de câmera atualmente selecionado."""
