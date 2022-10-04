c271f4634c6e2f0624c67c57790fc992873ad3ce
ayssag <ayssanaara@gmail.com>
2022-10-04 20:21:36 -0300

    Mensagem de espera de conexao com cliente

--- controller/vision/server.py
+++ controller/vision/server.py
@@ -5,18 +5,17 @@ class ServerPickle:
     def __init__(self):
 
         self.host = socket.gethostname()
-        self.port = 5001
+        self.port = 5002
 
         psocket = socket.socket() 
         psocket.bind((self.host, self.port))
-
+        print('Esperando cliente.....................................')
         psocket.listen(2)
         self.conn, self.address = psocket.accept()  
         print("Connection from: " + str(self.address))
         
     def send(self, data):
         message = pickle.dumps(data,-1)
-        print('tamanho message server: ', len(message))
         print("Enviando: ",data)
         self.conn.send(message)
     
